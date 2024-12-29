0d68619edd9ae518056e2b1762b7a06ccd2f7d4a
Minhaz03 <minhazulalamsohan2002@gmail.com>
2024-12-29 11:17:17 +0600

    NAv & Footer Added

---
+++ src/Pages/Shared/Navbar.jsx
@@ -0,0 +1,169 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  const links = (
+    <>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <details>
+          <summary>Authentications</summary>
+          <ul className="p-2">
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/register">Register</Link>
+            </li>
+          </ul>
+        </details>
+      </li>
+      <li>
+        <Link to=""></Link>
+      </li>
+
+      <li>
+        <Link to=""></Link>
+      </li>
+    </>
+  );
+
+  const [isScrolled, setIsScrolled] = useState(false);
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 0);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+  return (
+    <section>
+      <div className="mt-5">
+        <div
+          className={`navbar ${
+            isScrolled
+              ? "bg-white/70 backdrop-blur-lg shadow-md fixed top-0 left-0 w-full z-50 transition duration-300"
+              : "bg-transparent"
+          }`}
+        >
+          <div className="navbar-start">
+            <div className="dropdown">
+              <div
+                tabIndex={0}
+                role="button"
+                className="btn btn-ghost lg:hidden"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h8m-8 6h16"
+                  />
+                </svg>
+              </div>
+              <ul
+                tabIndex={0}
+                className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow font-bold"
+              >
+                {links}
+              </ul>
+            </div>
+            <a className="btn btn-ghost text-base md:text-xl italic h-full">
+              Bistro <br /> Boss
+            </a>
+          </div>
+          <div className="navbar-center hidden lg:flex">
+            <ul className="menu menu-horizontal px-1 font-bold">{links}</ul>
+          </div>
+          <div className="navbar-end">
+            <div className="dropdown dropdown-end">
+              <div
+                tabIndex={0}
+                role="button"
+                className="btn btn-ghost btn-circle avatar"
+              >
+                {/* user login image dropdoen and logout */}
+                <img
+                  className="w-11 rounded-full"
+                  src="https://i.ibb.co.com/82LpjHT/9439678.jpg"
+                  alt=""
+                />
+              </div>
+              <ul
+                tabIndex={0}
+                className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+              >
+                <Link to="/register" className="text-xl font-bold italic">
+                  <li>
+                    <h2>Register</h2>
+                  </li>
+                </Link>
+                <Link to="/login" className="text-xl font-bold italic">
+                  <li>
+                    <h2>Sign In</h2>
+                  </li>
+                </Link>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Navbar;
+
+// {
+/* <div className="w-10 rounded-full">
+                  {user && user.email ? (
+                    <div className="flex items-center gap-2">
+                      <img
+                        className="w-12 rounded-full"
+                        src="https://i.ibb.co.com/RSgnyVp/ajpeg.jpg"
+                        alt=""
+                      />
+                      {user.email}
+                    </div>
+                  ) : (
+                    <img
+                      className="w-11 rounded-full"
+                      src="https://i.ibb.co.com/82LpjHT/9439678.jpg"
+                      alt=""
+                    />
+                  )}
+                </div> */
+// }
+
+// {
+//   user ? (
+//     <>
+//       <button onClick={handleSignOut} className="btn font-bold italic">
+//         Sign Out/Log Out
+//       </button>
+//     </>
+//   ) : (
+//     <>
+//       <Link to="/register" className="text-xl font-bold italic">
+//         <li>
+//           <h2>Register</h2>
+//         </li>
+//       </Link>
+//       <Link to="/login" className="text-xl font-bold italic">
+//         <li>
+//           <h2>Sign In</h2>
+//         </li>
+//       </Link>
+//     </>
+//   );
+// }
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import {
-  RouterProvider,
-} from "react-router-dom";
-import { router } from './Routes/router';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./Routes/router";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>,
-)
+    <div className="max-w-screen-2xl mx-auto">
+      <RouterProvider router={router} />
+    </div>
+  </StrictMode>
+);
